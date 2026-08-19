5f50cf0c0eeb9847ad8912789aa8c1a0bb67be11
necto294 <stutaew@gmail.com>
2026-08-19 21:02:12 +0300

    database, alembic and .gitignore: Created engine and models, kipped migrations with alembic and remove old code in .gitignore

---
+++ database/engine.py
@@ -0,0 +1,22 @@
+import os
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncAttrs
+
+
+# Определяем корневую папку проекта и путь к файлу базы данных.
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+DB_PATH = os.path.join(BASE_DIR, "database", "database.db")
+
+
+# Формируем адрес асинхронного подключения к SQLite.
+DATABASE_URL = f"sqlite+aiosqlite:///{DB_PATH}"
+
+
+async_engine = create_async_engine(DATABASE_URL, echo=True)
+# Фабрика сессий не завершает объекты после фиксации транзакции.
+AsyncSessionLocal = async_sessionmaker(bind=async_engine, expire_on_commit=False)
+
+
+# Базовый класс для всех моделей SQLAlchemy.
+class Base(AsyncAttrs, DeclarativeBase):
+    pass
